5f7278091a6ff65cd7c95f53f58e2f8b39cd3fa7
Bulat <sadikovba@gmail.com>
2018-09-14 18:10:48 +0300

    Big head, Header, Footer

---
+++ index.php
@@ -0,0 +1,102 @@
+﻿<?php include('header.php'); ?>
+
+<div class="header-burger">
+	<button class="hamburger hamburger--slider">
+   <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+   </span>
+	</button>
+</div>
+
+
+<div class="header-sec">
+	<div class="container">
+		<div class="header">
+			<div class="header__logo">
+				<img src="img/header/logo-top-2x.png" alt="">
+			</div>
+			<nav class="header__nav">
+				<ul>
+					<li><a href="#" class="scrollto">Главная</a></li>
+					<li><a href="#" class="scrollto">кто мы?</a></li>
+					<li><a href="#" class="scrollto">виды помощи</a></li>
+					<li><a href="#" class="scrollto">Наши дела</a></li>
+					<li><a href="#" class="scrollto">рекомендации</a></li>
+					<li><a href="#" class="scrollto">контакты</a></li>
+				</ul>
+			</nav>
+			<div class="header__contact">
+				<a href="tel:84956468088" class="header__contact-phone">8 (495) 646-80-88</a>
+				<a href="mailto:info@bnplaw.ru" class="header__contact-mail">info@bnplaw.ru</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="mobile-menu-wrap">
+	<img src="img/header/logo-menu-2x.png" alt="">
+	<nav class="mobile-menu">
+		<ul>
+			<li><a href="#" class="scrollto">Главная</a></li>
+			<li><a href="#" class="scrollto">Кто мы?</a></li>
+			<li><a href="#" class="scrollto">Виды помощи</a></li>
+			<li><a href="#" class="scrollto">Наши дела</a></li>
+			<li><a href="#" class="scrollto">Рекомендации</a></li>
+			<li><a href="#" class="scrollto">Контакты</a></li>
+		</ul>
+	</nav>
+	<div class="mobile-menu__btn">
+		<a href="#" class="btn" data-src="#modal-call">Перезвоните мне</a>
+	</div>
+	<div class="mobile-menu__adress">
+		<span>Москва, ул.Неверовского д. 10 стр. 3А</span>
+	</div>
+</div>
+
+
+<section class="big-head-sec">
+	<div class="container">
+		<div class="big-head">
+			<div class="big-head__title">
+				<img src="img/header/logo-menu-2x.png" alt="">
+				<h1>Осуществляем защиту<small>от привлечения к&nbsp;субсидиарной ответственности</small></h1>
+				<span>Закажите бесплатную консультацию у&nbsp;нашего адвоката и&nbsp;вы узнаете свои шансы на победу</span>
+				<a href="#" class="btn">Бесплатная консультация</a>
+			</div>
+			<div class="big-head__facts">
+				<div class="big-head__fact">
+					<span>РФ</span>
+					<p>осуществляем защиту на&nbsp;всей территории Российской Федерации</p>
+				</div>
+				<div class="big-head__fact">
+					<span>120+</span>
+					<p>директоров остались с&nbsp;деньгами и&nbsp;хорошим настроением, благодаря нашей работе</p>
+				</div>
+				<div class="big-head__fact">
+					<span>11 520 000 р</span>
+					<p>средний размер субсидиарной ответственности, от&nbsp;которой мы защитили наших Доверителей</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<div class="footer-sec">
+	<div class="container">
+		<div class="footer">
+			<div class="footer__logo">
+				<img src="img/header/logo-top-2x.png" alt="">
+			</div>
+			<div class="footer__copy">
+				<p>Все права защищены. Башилов, Носков и партнеры, 2018</p>
+			</div>
+			<div class="footer__contact">
+				<a href="tel:84956468088" class="footer__contact-phone">8 (495) 646-80-88</a>
+				<a href="mailto:info@bnplaw.ru" class="footer__contact-mail">info@bnplaw.ru</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include('footer.php'); ?>
